26d53d4383c15e4ee43a3ed7fbe204f4faf9fd3c
Jubair Prince <jubairprince444@gmail.com>
2025-10-21 11:44:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5565292691-fc29b",
+  "appId": "1:802402226731:web:db9f778e61de73667c106e",
+  "apiKey": "AIzaSyCWGdWRZmFn_ft5ZFCLfzKsAOlLZ8SCH6I",
+  "authDomain": "studio-5565292691-fc29b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "802402226731"
+};
