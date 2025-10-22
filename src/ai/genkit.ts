87ef82fd518f4c1a2dd89384fb089e502c4df987
Jubair Prince <jubairprince444@gmail.com>
2025-10-22 16:23:58 +0000

    here for all ai support i will use google api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
   model: 'googleai/gemini-2.5-flash',
 });
